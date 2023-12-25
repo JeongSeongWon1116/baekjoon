a2beeea900790e72a8cd227a0691b0d685344748
JeongSeongWon1116 <122024715+JeongSeongWon1116@users.noreply.github.com>
2023-12-25 16:29:59 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <map>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <list>
+#include <math.h>
+#include <stack>
+#include <queue>
+#include <deque>
+using namespace std;
+typedef pair<int, int> p;
+
+int main(){
+    ios::sync_with_stdio(false);
+    int n;
+    scanf("%d", &n);
+    printf("%d", n*(n-1));
+}
