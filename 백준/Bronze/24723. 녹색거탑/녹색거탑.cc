dde7109cf6fa327c3d601bdc592a5d0faa00379d
JeongSeongWon1116 <122024715+JeongSeongWon1116@users.noreply.github.com>
2024-01-02 13:28:47 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  baekjoon
+//
+//  Created by SeongWon Jeong on 1/2/24.
+//
+
+#include <iostream>
+#include <cmath>
+int main(int argc, const char * argv[]) {
+    int n, result;
+    scanf("%d", &n);
+    result = pow(2, n);
+    printf("%d", result);
+}
