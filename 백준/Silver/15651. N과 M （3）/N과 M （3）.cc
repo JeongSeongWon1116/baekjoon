1145ba306633a85642354c24dad11b0f3e46245e
JeongSeongWon1116 <122024715+JeongSeongWon1116@users.noreply.github.com>
2024-01-11 13:02:49 +0900

    [Silver III] Title: N과 M (3), Time: 520 ms, Memory: 2212 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <map>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <list>
+#include <math.h>
+#include <stack>
+#include <queue>
+#include <deque>
+using namespace std;
+typedef pair<std::string, int> p;
+void permutation(int m, int n, std::set<int> set, std::vector<int> sel);
+int main(){
+    ios::sync_with_stdio(false);
+    int n, m;
+    std::set<int> set;
+    std::vector<int> sel;
+    scanf("%d %d", &n, &m);
+    permutation(m, n, set, sel);
+}
+void permutation(int m, int n, std::set<int> set, std::vector<int> sel){
+    if(m==1){
+        if(sel.empty()){
+            for(int i = 0; i<n; i++){
+                printf("%d\n", i+1);
+            }
+        }
+        else{
+            
+            for(int i = 0; i<n;i++){
+                sel.push_back(i);
+                
+                permutation(m-1, n, set, sel);
+                sel.pop_back();
+                
+            }
+        }
+    }
+    else if (m==0){
+        for(int i =0; i<sel.size();i++){
+            printf("%d ", sel[i] + 1);
+        }
+        printf("\n");
+    }
+    else{
+        
+        for(int i = 0; i<n;i++){
+            
+            
+            sel.push_back(i);
+            
+            permutation(m-1, n, set, sel);
+            sel.pop_back();
+            
+        }
+        
+    }
+
+}
